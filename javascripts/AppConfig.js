276a2e3f851756ce5992ab780cc365b61dd57025
Ellen Bailey <ellen@lndesigns.com>
2016-12-04 23:14:35 -0600

    local data displays

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,3 +1,36 @@
 "use strict";
 
 console.log("loaded AppConfig.js");
+
+// app.run(function(FIREBASE_CONFIG){
+// 	firebase.initializeApp(FIREBASE_CONFIG);
+// });
+
+// app.config(function($routeProvider){
+// 	$routeProvider
+// 		.when("/galleries", {
+// 			templateUrl: "/partials/galleries.html",
+// 			controller: "GalleriesCtrl"
+// 		})
+// 		.when("/gallery", {
+// 			templateUrl: "/partials/gallery.html",
+// 			controller: "GalleryCtrl"
+// 		})
+// 		.when("/about", { //:id can be named anything
+// 			templateUrl: "partials/about.html",
+// 			controller: "AboutCtrl"
+// 		})
+// 		.when("/search", { //:id can be named anything
+// 			templateUrl: "partials/search.html",
+// 			controller: "searchCtrl"
+// 		})
+// 		.when("/upload", { //:id can be named anything
+// 			templateUrl: "partials/upload.html",
+// 			controller: "searchCtrl"
+// 		})
+// 		.when("/login", { //:id can be named anything
+// 			templateUrl: "partials/login.html",
+// 			controller: "searchCtrl"
+// 		})
+// 		.otherwise("/galleries");
+// });
--- javascripts/factories/PhotoFactory.js
+++ javascripts/factories/PhotoFactory.js
@@ -1,3 +1,26 @@
 "use strict";
 
 console.log("loaded PhotoFactory.js");
+
+// app.factory("PhotoFactory", function ($q, $http, FIREBASE_CONFIG) {
+
+//     var getItemList = function () {
+//         return $q((resolve, reject) => {
+//             $http.get(`${FIREBASE_CONFIG.databaseURL}/photos.json`)
+//                 .success(function (response) {
+//                     let photos = [];
+//                     Object.keys(response).forEach(function (key) {
+//                         response[key].id = key;
+//                         photos.push(response[key]);
+//                     });
+//                     resolve(photos);
+//                 })
+//                 .error(function (errorResponse) {
+//                     reject(errorResponse);
+//                 });
+//         });
+//     };
+
+//     return { getPhotoList: getPhotoList };
+
+// });
